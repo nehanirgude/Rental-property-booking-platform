f2916ce9ee37a501fd078bee89a17b932b57266f
nehanirgude <nehanirgude3@gmail.com>
2024-10-07 10:12:17 +0530

    modify the database name

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 
-const MONGO_URL='mongodb://127.0.0.1:27017/test';
+const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
 
 main()
 .then(()=>{

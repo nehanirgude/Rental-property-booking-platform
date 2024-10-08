99bf594bd1e972d8dbdbf109961b72eb611713bd
nehanirgude <nehanirgude3@gmail.com>
2024-10-08 11:13:40 +0530

    connecting with dbs

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+// const Listing=require("./models/listing.js");
 
-const MONGO_URL='mongodb://127.0.0.1:27017/wanderlust';
+main().then((res=>{
+  console.log("connection succesfull");
+}))
+.catch(err => console.log(err));
 
-main()
-.then(()=>{
-    console.log("connected to DB");
-})
-.catch((err)=>{
-    console.log(err);
-});
+async function main() {
+await mongoose.connect('mongodb://localhost:27017/wanderlust');
 
-async function main(){
-    await mongoose.connect(MONGO_URL);
+// use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 app.get("/",(req,res)=>{
     res.send("root is working");
-})
+});
+
+app.get("/testListing")
 
 app.listen(8080,()=>{
     console.log("app is listening to port 8080");
--- models/listing.js
+++ models/listing.js
@@ -2,9 +2,15 @@ const mongoose=require(mongoose);
 const Schema=mongoose.Schema;
 
 const ListingSchema=new Schema({
-    title:String,
+    title:{
+        type:String,
+        required:true,
+    },
     description:String,
-    image:String,
+    image:{
+        type:String,
+        set: (v) =>v ===""? "https://unsplash.com/photos/a-pool-of-water-surrounded-by-rocks-and-trees-zo_udYMcaVc":v,
+    },
     price:Number,
     location:String,
     country:String,
@@ -12,4 +18,4 @@ const ListingSchema=new Schema({
 });
 
 const Listing=mongoose.model("Listing",ListingSchema);
-module.exports=Listing;
+module.export=Listing;

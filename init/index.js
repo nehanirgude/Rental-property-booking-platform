807f281e9faf75d452a21a1733404488a76b5d1d
nehanirgude <nehanirgude3@gmail.com>
2024-10-12 14:23:22 +0530

    database file

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose=require("mongoose");
+const initData=require("./data.js");
+const Listing = require('../models/listing.js');
+
+
+
+main().then((res=>{
+    console.log("connection succesfull");
+  }))
+  .catch(err => console.log(err));
+  
+  async function main() {
+  await mongoose.connect('mongodb://localhost:27017/wanderlust');
+  
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  }
+  
+
+  const initDB=async()=>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data); //initData is object and we want to access key data of that object
+    console.log("data was initialize");
+  }
+
+  initDB();
+  

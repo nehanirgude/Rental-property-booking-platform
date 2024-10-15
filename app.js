423fa2dc3139cb96ee0c0b823eb4ab173052320b
nehanirgude <nehanirgude3@gmail.com>
2024-10-15 08:24:19 +0530

    some new files for crud operations

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const app=express();
 const mongoose=require('mongoose');
 const Listing=require("./models/listing.js");
 const path=require("path");
+app.use(express.json());
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 
 main().then((res=>{
@@ -51,6 +53,47 @@ app.get("/listings",async(req,res)=>{
    const allListings = await Listing.find({});
    res.render("./listings/index.ejs",{allListings});
 });
+
+//create or new route
+
+app.get("/listings/new",(req,res)=>{
+  res.render("./listings/new.ejs");
+});
+
+
+//create route
+
+app.post("/listings/listings", async (req, res) => {
+  const newListing= new Listing(req.body.listing);
+  await newListing.save();
+  console.log(req.body);
+  res.redirect("/listings");
+  // Further processing...
+});
+
+// app.post("/listings",async(req,res)=>{
+//   // let {title,description,image,price,country,location} =req.body;
+
+//   let listing=req.body.listing;
+//   console.log(req.body);
+  
+// });
+
+//show route-which will shows all the details of listing
+
+app.get("/listings/:id",async(req,res)=>{
+  let {id}=req.params;
+  const listing =await Listing.findById(id);
+  res.render("./listings/show.ejs",{listing});
+});
+
+
+
+
+
+
+
+
 
 app.listen(8080,()=>{
     console.log("app is listening to port 8080");
